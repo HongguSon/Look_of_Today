1bcb7654b1d1e5d99512f0e0506def6d3daca2b0
gardening-y <60008786@naver.com>
2023-02-02 21:41:06 +0900

    [chore] url 수정

--- server/apps/user/urls.py
+++ server/apps/user/urls.py
@@ -6,9 +6,6 @@ app_name = "user"
 urlpatterns = [
   path("signup/", views.signup, name="signup"),
   path("logout/", views.logout, name="logout"),
-<<<<<<< Updated upstream
   path("login/", views.log_in, name="login"),
-=======
   path("profile_update/", views.profile_update, name="profile_update"),
->>>>>>> Stashed changes
 ]
