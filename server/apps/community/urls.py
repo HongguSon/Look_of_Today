e206ba86d6a3c24ec6924f59b4cce42423821c94
Chaewon <ksdyys3@naver.com>
2023-02-02 23:37:52 +0900

    [feat] post 상세, 삭제

--- server/apps/community/urls.py
+++ server/apps/community/urls.py
@@ -5,5 +5,8 @@ app_name = "community"
 
 urlpatterns = [
   path("", views.community_main, name="community_main"),
-  path("create",views.post_create,name="post_create")
+  path("create",views.post_create,name="post_create"),
+  path("<int:pk>", views.detail, name='detail'),
+  path("<int:pk>/update", views.update, name='update'),
+  path("<int:pk>/delete", views.delete,name='delete'),
 ]
--- server/apps/community/views.py
+++ server/apps/community/views.py
@@ -8,6 +8,14 @@ def community_main(request):
         'post_list':post_list
         }   
     return render(request,'community\community.html',context=context)
+def detail(request,pk):
+    post = Post.objects.get(id=pk)
+    print(post)
+    context={
+        "post": post,
+    }
+    return render(request,'community\detail.html',context=context)
+
 
 def post_create(request:HttpRequest,*args, **kwargs):
     clothes_list=Clothes.objects.all()
@@ -24,3 +32,12 @@ def post_create(request:HttpRequest,*args, **kwargs):
         "clothes_list": clothes_list,
     }
     return render(request, 'community\post_create.html',context=context)
+
+def update(request:HttpRequest,*args, **kwargs):
+    return render(request, 'community\post_create.html')
+
+def delete(request:HttpRequest, pk, *args, **kwargs):
+    if request.method == "POST":
+        post = Post.objects.get(id=pk)
+        post.delete()
+    return redirect("/")
