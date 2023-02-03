710493b053feab189929b666716902d1d0677f5d
soozzang <soozzang@inha.edu>
2023-02-03 12:56:06 +0900

    [feat] 수정

--- server/apps/community/views.py
+++ server/apps/community/views.py
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-# from django.http import JsonResponse
-# from django.shortcuts import render
-# from server.apps.main.models import Comment
-# import json
-
-# # Create your views here.
-# def community_main(request):
-#     return render(request, 'community/community.html')
-
-# def post_create(request):
-#     return render(request, 'community/post_create.html')
-
-# def post_detail(request):
-#     return render(request, 'community/post_detail.html')
-
-# # @csrf_exempt
-# def comment(request):
-#     jsonobject = json.loads(request.body)
-
-#     comment = Comment.objects.create(
-#         title=jsonobject.get('boardId'),
-#         author=jsonobject.get('memberId'),
-#         content=jsonobject.get('content'))
-#     comment.save()
-
-#     context = {
-#         'author' : comment.author,
-#         'content' : comment.content,
-#         'create_Date' : comment.create_date,
-#         'update_Date' : comment.update_date,
-#     }
-    
-#     return JsonResponse(context);
-=======
-=======
->>>>>>> garden
-from django.shortcuts import render, redirect
-import json
-from server.apps.main.models import Post, Clothes, Comment
-=======
 import json
 from django.shortcuts import render, redirect
 from server.apps.main.models import Post,Clothes,Comment
@@ -50,7 +7,6 @@ from django.http import Http404
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.http.request import HttpRequest
->>>>>>> chaewon
 from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
@@ -66,18 +22,7 @@ def community_main(request):
     context={
         'post_list':post_list
         }   
-    return render(request,'community\community.html',context=context)
-<<<<<<< HEAD
-  
-=======
-
-def detail(request,pk):
-    post = Post.objects.get(id=pk)
-    print(post)
-    context={
-        "post": post,
-    }
-    return render(request,'community\post_detail.html',context=context)
+    return render(request,'community/community.html',context=context)
 
 def delete(request:HttpRequest, pk, *args, **kwargs):
     if request.method == "POST":
@@ -99,27 +44,22 @@ def likes(request, pk):
         # return redirect('accouts:login')위에거 대신 이거 떠야함! 나중에 로그인 합치고!!
     return render(request, 'community:detail.html')
 
->>>>>>> chaewon
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
-  model = Post
-  fields = ['main_img', 'title', 'clothes']
-  
-  template_name = 'community/post_create.html'
+    model = Post
+    fields = ['main_img', 'title', 'clothes']
 
-  def test_func(self):
-    return self.request.user.is_superuser or self.request.user.is_staff
+    template_name = 'community/post_create.html'
 
-  def form_valid(self, form):
-    current_user = self.request.user
-    if current_user.is_authenticated and (current_user.is_superuser or current_user.is_staff):
-      form.instance.author = current_user
-      return super(PostCreate, self).form_valid(form)
-    else:
-      return redirect('community:community_main')
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> garden
-=======
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_superuser or current_user.is_staff):
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('community:community_main')
 
 def post_create(request):
     return render(request, 'community/post_create.html')
@@ -131,8 +71,7 @@ def post_detail(request, pk, *args, **kwargs):
     }
     return render(request, 'community/post_detail.html', context=context)
 
-=======
->>>>>>> chaewon
+
 @csrf_exempt
 def comment_ajax(request, *args, **kwargs):
     data = json.loads(request.body)
@@ -152,9 +91,7 @@ def comment_ajax(request, *args, **kwargs):
     }
     
     return JsonResponse(context)
-<<<<<<< HEAD
->>>>>>> garden
-=======
+
 
 class update(LoginRequiredMixin,UpdateView):
   model = Post
@@ -167,4 +104,4 @@ class update(LoginRequiredMixin,UpdateView):
             return super(update, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
->>>>>>> chaewon
+
