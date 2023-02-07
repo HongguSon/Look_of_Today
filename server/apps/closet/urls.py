92fc747dee1e470b196c57b35a1ea510392d69d3
Chaewon <ksdyys3@naver.com>
2023-02-07 15:17:13 +0900

    [chore] 모델 수정

--- server/apps/closet/urls.py
+++ server/apps/closet/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
   path("", views.closet_main, name="closet_main"),
   path("2/", views.closet_main2, name="closet_main2"),
   # path("Post_create/",views.PostCreate.as_view(),name="post_create"),
-  path("create/",views.ClothesCreate.as_view(),name="clothes_create"),
+  # path("create/",views.ClothesCreate.as_view(),name="clothes_create"),
 ]
--- server/apps/main/admin.py
+++ server/apps/main/admin.py
@@ -4,4 +4,10 @@ from .models import *
 
 admin.site.register(Post)
 admin.site.register(Comment)
-admin.site.register(Clothes)
+admin.site.register(Top)
+admin.site.register(Bottom)
+admin.site.register(Shose)
+admin.site.register(Acc)
+admin.site.register(Outter)
+admin.site.register(Talk)
+
