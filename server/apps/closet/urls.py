53a16065dcce89c9bf908906d24b77b5acf482a2
gardening-y <60008786@naver.com>
2023-02-07 21:49:09 +0900

    [feat] clothes create, our_closet

--- server/apps/closet/urls.py
+++ server/apps/closet/urls.py
@@ -5,7 +5,11 @@ app_name = "closet"
 
 urlpatterns = [
   path("", views.closet_main, name="closet_main"),
+  path("our_closet/", views.our_closet, name="our_closet"),
   path("2/", views.closet_main2, name="closet_main2"),
-  # path("Post_create/",views.PostCreate.as_view(),name="post_create"),
-  # path("create/",views.ClothesCreate.as_view(),name="clothes_create"),
+  path("create/top/",views.TopCreate.as_view(),name="top_create"),
+  path("create/bottom/",views.BottomCreate.as_view(),name="bottom_create"),
+  path("create/outer/",views.OuterCreate.as_view(),name="outer_create"),
+  path("create/acc/",views.AccCreate.as_view(),name="acc_create"),
+  path("create/shoes/",views.ShoesCreate.as_view(),name="shoes_create"),
 ]
--- server/apps/closet/views.py
+++ server/apps/closet/views.py
@@ -38,3 +38,96 @@ def closet_main2(request):
 #       return super(ClothesCreate, self).form_valid(form)
 #     else:
 #       return redirect('closet:closet_main')
+
+def our_closet(request, *args, **kwargs):
+    post_list = Post.objects.filter(open=True)
+
+    context={
+        'post_list' : post_list,
+        }   
+    return render(request,'closet/our_closet.html',context=context)
+
+class TopCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+  model = Top
+  fields = ['img', 'title', 'buying']
+  
+  template_name = 'closet/clothes_create.html'
+
+  def test_func(self):
+    return self.request.user
+
+  def form_valid(self, form):
+    current_user = self.request.user
+    if current_user.is_authenticated:
+      form.instance.author = current_user
+      return super(TopCreate, self).form_valid(form)
+    else:
+      return redirect('closet:closet_main')
+    
+class BottomCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+  model = Bottom
+  fields = ['img', 'title', 'buying']
+  
+  template_name = 'closet/clothes_create.html'
+
+  def test_func(self):
+    return self.request.user
+
+  def form_valid(self, form):
+    current_user = self.request.user
+    if current_user.is_authenticated:
+      form.instance.author = current_user
+      return super(BottomCreate, self).form_valid(form)
+    else:
+      return redirect('closet:closet_main')
+    
+class OuterCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+  model = Outer
+  fields = ['img', 'title', 'buying']
+  
+  template_name = 'closet/clothes_create.html'
+
+  def test_func(self):
+    return self.request.user
+
+  def form_valid(self, form):
+    current_user = self.request.user
+    if current_user.is_authenticated:
+      form.instance.author = current_user
+      return super(OuterCreate, self).form_valid(form)
+    else:
+      return redirect('closet:closet_main')
+    
+class AccCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+  model = Acc
+  fields = ['img', 'title', 'buying']
+  
+  template_name = 'closet/clothes_create.html'
+
+  def test_func(self):
+    return self.request.user
+
+  def form_valid(self, form):
+    current_user = self.request.user
+    if current_user.is_authenticated:
+      form.instance.author = current_user
+      return super(AccCreate, self).form_valid(form)
+    else:
+      return redirect('closet:closet_main')
+    
+class ShoesCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+  model = Shoes
+  fields = ['img', 'title', 'buying']
+  
+  template_name = 'closet/clothes_create.html'
+
+  def test_func(self):
+    return self.request.user
+
+  def form_valid(self, form):
+    current_user = self.request.user
+    if current_user.is_authenticated:
+      form.instance.author = current_user
+      return super(ShoesCreate, self).form_valid(form)
+    else:
+      return redirect('closet:closet_main')
