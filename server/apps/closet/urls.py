a323e5d9ed787ca448e2d0ae543ba4e84c5fa398
soozzang <soozzang@inha.edu>
2023-02-07 21:10:47 +0900

    [chore] 수현- 모델수정, 나만의 옷장 수정

--- server/apps/closet/urls.py
+++ server/apps/closet/urls.py
@@ -6,6 +6,12 @@ app_name = "closet"
 urlpatterns = [
   path("", views.closet_main, name="closet_main"),
   path("2/", views.closet_main2, name="closet_main2"),
+  path("outer/", views.outer_list, name="outer_list"),
+  path("top/", views.top_list, name="top_list"),
+  path("bottom/", views.bottom_list, name="bottom_list"),
+  path("shoes/", views.shoes_list, name="shoes_list"),
+  path("acc/", views.acc_list, name="acc_list"),
   # path("Post_create/",views.PostCreate.as_view(),name="post_create"),
   # path("create/",views.ClothesCreate.as_view(),name="clothes_create"),
+  path("outer_create/",views.OuterCreate.as_view(),name="outer_create"),
 ]
--- server/apps/closet/views.py
+++ server/apps/closet/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render, redirect
 from server.apps.main.models import *
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import CreateView, UpdateView
+from itertools import chain
 
 # Create your views here.
 def closet_main(request):
-    # clothes_list = Clothes.objects.all() 
-    # clothes_count = Clothes.objects.all().count()
-    context={
-        # 'clothes_list' : clothes_list,
-        # 'clothes_count' : clothes_count,
-        }   
-    return render(request,'closet/closet_main.html',context=context)
+    qs1 = Outer.objects.filter(author=request.user)
+    qs2 = Top.objects.filter(author=request.user)
+    qs3 = Bottom.objects.filter(author=request.user)
+    qs4 = Shoes.objects.filter(author=request.user)
+    qs5 = Acc.objects.filter(author=request.user)
+    clothes_list = []
+    clothes_all = qs1.union(qs2, qs3, qs4, qs5)
+    # clothes_list = clothes_all.objects.filter(author=request.user)
+    context ={
+        'clothes_list': clothes_all
+    }
+    return render(request,'closet/closet_main.html', context=context)
 
 def closet_main2(request):
     this_user = request.user
@@ -22,19 +28,71 @@ def closet_main2(request):
         }   
     return render(request,'closet/closet_main2.html',context=context)
 
-# class ClothesCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
-#   model = Clothes
-#   fields = ['img','title', 'category', 'buying']
-  
-#   template_name = 'closet/clothes_create.html'
 
-#   def test_func(self):
-#     return self.request.user
+class OuterCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Outer
+    fields = ['img','title']
 
-#   def form_valid(self, form):
-#     current_user = self.request.user
-#     if current_user.is_authenticated:
-#       form.instance.author = current_user
-#       return super(ClothesCreate, self).form_valid(form)
-#     else:
-#       return redirect('closet:closet_main')
+    template_name = 'closet/outer_create.html'
+
+    def test_func(self):
+        return self.request.user
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(OuterCreate, self).form_valid(form)
+        else:
+            return redirect('closet:outer_list')
+
+def outer_list(request):
+    clothes_list = Outer.objects.filter(author=request.user)
+
+    return render(
+        request,
+        'closet/closet_main.html',
+        {
+            'clothes_list': clothes_list,
+        })
+
+def top_list(request):
+    clothes_list = Top.objects.filter(author=request.user)
+
+    return render(
+        request,
+        'closet/closet_main.html',
+        {
+            'clothes_list': clothes_list,
+        })
+
+def bottom_list(request):
+    clothes_list = Bottom.objects.filter(author=request.user)
+
+    return render(
+        request,
+        'closet/closet_main.html',
+        {
+            'clothes_list': clothes_list,
+        })
+
+def shoes_list(request):
+    clothes_list = Shoes.objects.filter(author=request.user)
+
+    return render(
+        request,
+        'closet/closet_main.html',
+        {
+            'clothes_list': clothes_list,
+        })
+
+def acc_list(request):
+    clothes_list = Acc.objects.filter(author=request.user)
+
+    return render(
+        request,
+        'closet/closet_main.html',
+        {
+            'clothes_list': clothes_list,
+        })
+
