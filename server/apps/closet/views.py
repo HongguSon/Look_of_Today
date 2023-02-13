1df1e5207d29f7ad91c57cf1976be2f71f2a9932
Chaewon <ksdyys3@naver.com>
2023-02-13 09:14:45 +0900

    Auto stash before rebase of "origin/feat/gokun2"

--- server/apps/closet/views.py
+++ server/apps/closet/views.py
@@ -4,14 +4,31 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import CreateView, UpdateView
 from django.views.decorators.http import require_POST
 from itertools import chain
+from rembg import remove
+from PIL import Image
+import os
 
 
 # Create your views here.
 # REVIEW : mysql 파일 삭제 요망
 def closet_main(request, *args, **kwargs):
   clothes_list = Clothes.objects.filter(author=request.user)
+  clothes =Clothes.objects.get(pk=1)
+  clothes_url=clothes.img.url
+  new_url_print=clothes_url #출력을 위한 경로
+  clothes_url= "C:\\Users\ksdyy\OneDrive\바탕 화면\P\Look_of_Today\server"+clothes_url
+  input=Image.open(clothes_url)
+  output=remove(input)
+  dir,file = os.path.split(clothes_url)
+  new_url=dir+'/redu_'+file
+  output=output.convert("RGB")
+  output.save(new_url) #파일에는 이렇게 해야 같은 폴더에 들어감..!
+  n_dir,n_file=os.path.split(new_url_print)
+  new_url_print=n_dir+'/redu_'+n_file
   context = {
     'clothes_list': clothes_list,
+    'clothes_url':clothes_url,
+    'new_url':new_url_print,
   }
   return render(request,'closet/closet_main.html', context=context)
 
@@ -103,15 +120,104 @@ def create_clothes(request, *args, **kwargs):
     return redirect('closet:closet_main')
   return render(request, "closet/clothes_create.html") 
 
-@require_POST
-def clothes_likes(request, pk, *args, **kwargs):
-  if request.user.is_authenticated:
-    clothes = get_object_or_404(Clothes, pk=pk)
-    users = clothes.likes.all()
-    if users.filter(pk=request.user.pk).exists():
-      clothes.likes.remove(request.user)
+  template_name = 'closet/clothes_create.html'
+
+  def test_func(self):
+    return self.request.user
+
+  def form_valid(self, form):
+    current_user = self.request.user
+    if current_user.is_authenticated:
+      form.instance.author = current_user
+      # input = Image.open(form.instance.img.url)
+      # output = remove(input)
+      # output.save(form.instance.rem_img)
+      return super(TopCreate, self).form_valid(form)
     else:
-      clothes.likes.add(request.user)
-    return redirect('closet:closet_main')
-    # return redirect('accouts:login')위에거 대신 이거 떠야함! 나중에 로그인 합치고!!
-  return render(request, 'closet/our_closet.html')
+      return redirect('closet:closet_main')
+
+  
+
+class BottomCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+  model = Bottom
+  fields = ['img', 'title', 'buying']
+
+  template_name = 'closet/clothes_create.html'
+
+  def test_func(self):
+    return self.request.user
+
+  def form_valid(self, form):
+    current_user = self.request.user
+    if current_user.is_authenticated:
+      form.instance.author = current_user
+      return super(BottomCreate, self).form_valid(form)
+    else:
+      return redirect('closet:closet_main')
+
+class OuterCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+  model = Outer
+  fields = ['img', 'title', 'buying']
+
+  template_name = 'closet/clothes_create.html'
+
+  def test_func(self):
+    return self.request.user
+
+  def form_valid(self, form):
+    current_user = self.request.user
+    if current_user.is_authenticated:
+      form.instance.author = current_user
+      return super(OuterCreate, self).form_valid(form)
+    else:
+      return redirect('closet:closet_main')
+
+class AccCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+  model = Acc
+  fields = ['img', 'title', 'buying']
+
+  template_name = 'closet/clothes_create.html'
+
+  def test_func(self):
+    return self.request.user
+
+  def form_valid(self, form):
+    current_user = self.request.user
+    if current_user.is_authenticated:
+      form.instance.author = current_user
+      return super(AccCreate, self).form_valid(form)
+    else:
+      return redirect('closet:closet_main')
+
+class ShoesCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+  model = Shoes
+  fields = ['img', 'title', 'buying']
+
+  template_name = 'closet/clothes_create.html'
+
+  def test_func(self):
+    return self.request.user
+
+  def form_valid(self, form):
+    current_user = self.request.user
+    if current_user.is_authenticated:
+      form.instance.author = current_user
+      return super(ShoesCreate, self).form_valid(form)
+    else:
+      return redirect('closet:closet_main')
+
+# class ClothesCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+#   context_object_name = 'clothes'
+#   template_name = 'closet/clothes_create.html'
+
+#   def dispatch(self, request, *args, **kwargs):
+#     clothes_kind = kwargs.get('clothes', None)
+#     if clothes_kind == 'dog':
+#       self.model = Dog
+#     elif clothes_kind == 'cat':
+#       self.model = Cat
+
+#     return super(AnimalDetailView, self).dispatch(request, *args, **kwargs)
+
+#   def get_queryset(self):
+#       return self.model.objects.filter()
