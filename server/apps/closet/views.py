ab44639a7d01a24942022a8e72cfbfe1492471d0
HongguSon <honggus0830@gmail.com>
2023-02-17 22:11:30 +0900

    [feat] 구매링크 구현 완료

--- server/apps/closet/views.py
+++ server/apps/closet/views.py
@@ -188,18 +188,54 @@ def post_like_list(request, *args, **kwargs):
   return render(request,'closet/closet_main.html',context=context)
   
 def buylink(request, pk, *args, **kwargs):
-  flag=False
   cloth = Clothes.objects.filter(id=pk)
-  url = cloth[0].img.path
   context = {
     'cloth' : cloth[0],
+    'fastflag' : False,
+    'exactflag' : False,
   }
-  # if flag:
-  #   output = Search(file_path=url)['similar']
-  #   context['output'] = output
-  # # else:
-  # if request.method == "POST":
-  #   choice = request.POST
-    
-    
+  if request.method == "POST":
+    choice = list(request.POST.values())[-1]
+    if choice == '빠른 검색':
+      output = Search(file_path=cloth[0].img.path)
+      link = output['similar']
+      context['link']=link
+      context['fastflag'] = True
+    elif choice == '정확한 검색':
+      options = Options()
+      options.add_argument('--headless')
+      options.add_argument("window-size=1400,850")
+      driver = webdriver.Chrome('/usr/local/bin/chromedriver',options=options)
+      driver.get('http://www.google.hr/imghp')
+      driver.implicitly_wait(10)
+      elem = driver.find_element(By.CLASS_NAME, 'Gdd5U')
+      elem.click()
+      driver.find_element(By.NAME, 'encoded_image').send_keys(cloth[0].img.path)
+      # items = driver.find_element(By.CSS_SELECTOR, 'div.Vd9M6.abDdKd.xuQ19b')
+      titles_finder = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'div.Vd9M6.abDdKd.xuQ19b')))
+      
+      search_informations_list = []
+      for num, title in enumerate(titles_finder):
+        search_info_list = []
+        text_info = title.text.split('\n')
+        for element in text_info:
+          if '₩' in element:
+            search_info_list.append(element)
+            break
+        else:
+          search_info_list.append('가격정보없음')
+          
+        a_tag_finder = title.find_element(By.XPATH, ".//a")
+        img_tag_finder = a_tag_finder.find_element(By.XPATH, ".//div[1]/div[1]/div[1]/img")
+        exact_url = a_tag_finder.get_dom_attribute('href')
+        exact_src = img_tag_finder.get_dom_attribute('src')
+        
+        search_info_list.append(exact_url)
+        search_info_list.append(exact_src)
+        search_informations_list.append(search_info_list)
+        #리스트 요소는 각각 리스트 : [가격, 구매링크, 이미지src] 로 구성되어있음
+        if num >= 3:
+          break
+      context['search_informations_list']=search_informations_list
+      context['exactflag'] = True
   return render(request,'closet/buylink.html', context=context)
