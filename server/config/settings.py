3891a3282cf506c4dde506049fb549e71e00db6c
soozzang <soozzang@inha.edu>
2023-02-20 05:27:29 +0900

    [feat] 배포실험.

--- server/config/settings.py
+++ server/config/settings.py
@@ -129,20 +129,23 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_DIR = [
-    os.path.join(BASE_DIR,'static'),
-]
+# STATIC_DIR = [
+#     os.path.join(BASE_DIR,'static'),
+# ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
 
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
+
+#STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
