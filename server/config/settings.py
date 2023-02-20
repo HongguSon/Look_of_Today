13a2734e783c813ad14703b08a27e5818f7e0bd3
soozzang <soozzang@inha.edu>
2023-02-20 15:09:06 +0900

    [feat] 배포 쉼표삭제~

--- server/config/settings.py
+++ server/config/settings.py
@@ -134,7 +134,7 @@ if DEBUG:
         os.path.join(BASE_DIR, 'static'),
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static/'),
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 
