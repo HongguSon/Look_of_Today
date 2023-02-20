bae6a824c9fbb2996ce6b845e734db57c70a72c4
soozzang <soozzang@inha.edu>
2023-02-20 12:36:46 +0900

    [feat] 배포 실험 DEBUG오류 관련

--- server/config/settings.py
+++ server/config/settings.py
@@ -129,21 +129,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_DIR = [
-#     os.path.join(BASE_DIR,'static'),
-# ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
-
-# STATICFILES_DIRS = [
-#     STATIC_DIR,
-# ]
-
-#STATICFILES_DIRS = [BASE_DIR / "static"]
+if DEBUG:
+    STATICFILES_DIR = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/'),
 
 
 
-MEDIA_URL = '/media/'
+
+MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
